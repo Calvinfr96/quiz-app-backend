40305d76928213fc7d64a92e88ab4862d09f6f20
calvin <calvin.francis96@outlook.com>
2021-10-01 14:22:31 -0400

    Add validations for Quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,7 @@ class Quiz < ApplicationRecord
     has_many :questions
     has_many :attempts
     has_many :users, through: :attempts
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
