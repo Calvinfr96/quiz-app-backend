90bfa4789794bac5c239ce7aa96e30d9ae107600
calvin <calvin.francis96@outlook.com>
2021-10-01 17:34:22 -0400

    Add validations for Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ApplicationRecord
     belongs_to :quiz
+
+    validates :prompt, presence: true
+    validates :prompt, uniqueness: true
+    validates :answers, presence: true
+    validates :correct_index, presence: true
 end
