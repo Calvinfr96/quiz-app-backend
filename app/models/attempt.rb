2bc4e327fcf30a2c0d898e527c408130fd2e58f4
calvin <calvin.francis96@outlook.com>
2021-10-01 18:25:02 -0400

    Add validations for Attempt model

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,4 +1,9 @@
 class Attempt < ApplicationRecord
     belongs_to :quiz
     belongs_to :user
+
+    validates :number_correct, presence: true
+    validates :passed?, presence: true
+    validates :user_id, presence: true
+    validates :quiz_id, presence: true
 end
