2dd96bf19b67c46d8832a6b983ddb0338defe3b8
calvin <calvin.francis96@outlook.com>
2021-10-01 18:47:43 -0400

    Ensure dependent data is properly destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :attempts
+    has_many :attempts, dependent: :destroy
     has_many :quizzes, through: :attempts
     validates :name, presence: true
     validates :name, uniqueness: true
