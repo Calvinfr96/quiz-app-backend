f9391f2636687a6b5b34140e16a94501e909a5dd
calvin <calvin.francis96@outlook.com>
2021-10-08 11:13:50 -0400

    Add password_digest comlumn to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_220002) do
+ActiveRecord::Schema.define(version: 2021_10_08_150915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_10_01_220002) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "attempts", "quizzes"
