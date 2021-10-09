dbbf1f4ea53611700f659eb6f46eefed78edf881
calvin <calvin.francis96@outlook.com>
2021-10-08 20:03:46 -0400

    Add routing for user login

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -2,10 +2,10 @@ class AuthorizationController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-        @user = User.find_by(name: params[:username])
+        @user = User.find_by(name: params[:name])
         if @user && @user.authenticate(params[:password])
-            token = encode_token(user_id: @user.id)
-            render json: {user: @user, jwt: @token}, status: :accepted
+            @token = encode_token({user_id: @user.id})
+            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :accepted
         else
             render json: {error: 'Invalid Username or Password'}, status: :unauthorized
         end
